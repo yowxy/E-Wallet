d823849f71272004e31ce6d2861aebe18951c6c2
yowxy <iklilblender@gmail.com>
2024-10-18 13:18:01 +0700

    create Floating Action Button in HomePage

--- lib/ui/pages/home_pages.dart
+++ lib/ui/pages/home_pages.dart
@@ -10,50 +10,69 @@ class HomePage extends StatelessWidget {
       
       backgroundColor: lightBackgroundColor,
     
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: blueColor,
-        unselectedItemColor: blackColor,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        selectedLabelStyle: blueTextStyle.copyWith(
-          fontSize: 10,
-          fontWeight: medium,
-        ),
-        unselectedLabelStyle: blackTextStyle.copyWith(
-          fontSize: 10,
-          fontWeight: medium,
-        ),
-        items: [
-        BottomNavigationBarItem(
-          icon: Image.asset(
-            'assets/ic_overview.png',
-            color: blueColor,
-            width: 20,
+      bottomNavigationBar: BottomAppBar(
+        color: whiteColor,
+        shape: const CircularNotchedRectangle(),
+        clipBehavior: Clip.antiAlias,
+        notchMargin: 6,
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: whiteColor,
+          elevation: 0,
+          selectedItemColor: blueColor,
+          unselectedItemColor: blackColor,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          selectedLabelStyle: blueTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
           ),
-          label: "Overview",
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset(
-            'assets/ic_history.png',
-            width: 20,
+          unselectedLabelStyle: blackTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
           ),
-          label: "History",
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset(
-            'assets/ic_statistc.png',
-            width: 20,
+          items: [
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/ic_overview.png',
+              color: blueColor,
+              width: 20,
+            ),
+            label: "Overview",
           ),
-          label: "Statistic",
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset(
-            'assets/ic_reward.png',
-            width: 20,
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/ic_history.png',
+              width: 20,
+            ),
+            label: "History",
           ),
-          label: "Reward",
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/ic_statistc.png',
+              width: 20,
+            ),
+            label: "Statistic",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/ic_reward.png',
+              width: 20,
+            ),
+            label: "Reward",
+          ),
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: purpleColor,
+        shape: CircleBorder(),
+        child: Image.asset(
+          'assets/ic_plus_circle.png',
+          width: 24,
+          ),
         ),
-      ]),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
