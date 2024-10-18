802a6beac0231d61b7f23c52c8ff33d8fb6a3917
yowxy <iklilblender@gmail.com>
2024-10-18 11:06:08 +0700

    slicing HomePage bottom Navigation

--- lib/ui/pages/home_pages.dart
+++ lib/ui/pages/home_pages.dart
@@ -1,3 +1,4 @@
+import "package:e_wallet/shared/theme.dart";
 import "package:flutter/material.dart";
 
 class HomePage extends StatelessWidget {
@@ -5,6 +6,54 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      
+      backgroundColor: lightBackgroundColor,
+    
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: blueColor,
+        unselectedItemColor: blackColor,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        selectedLabelStyle: blueTextStyle.copyWith(
+          fontSize: 10,
+          fontWeight: medium,
+        ),
+        unselectedLabelStyle: blackTextStyle.copyWith(
+          fontSize: 10,
+          fontWeight: medium,
+        ),
+        items: [
+        BottomNavigationBarItem(
+          icon: Image.asset(
+            'assets/ic_overview.png',
+            color: blueColor,
+            width: 20,
+          ),
+          label: "Overview",
+        ),
+        BottomNavigationBarItem(
+          icon: Image.asset(
+            'assets/ic_history.png',
+            width: 20,
+          ),
+          label: "History",
+        ),
+        BottomNavigationBarItem(
+          icon: Image.asset(
+            'assets/ic_statistc.png',
+            width: 20,
+          ),
+          label: "Statistic",
+        ),
+        BottomNavigationBarItem(
+          icon: Image.asset(
+            'assets/ic_reward.png',
+            width: 20,
+          ),
+          label: "Reward",
+        ),
+      ]),
+    );
   }
 }
