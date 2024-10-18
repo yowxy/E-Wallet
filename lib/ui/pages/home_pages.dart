75ada99be76127989a55823e06238858504e032e
yowxy <iklilblender@gmail.com>
2024-10-18 14:24:52 +0700

    create Build Profile

--- lib/ui/pages/home_pages.dart
+++ lib/ui/pages/home_pages.dart
@@ -7,72 +7,140 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       backgroundColor: lightBackgroundColor,
-    
       bottomNavigationBar: BottomAppBar(
         color: whiteColor,
         shape: const CircularNotchedRectangle(),
         clipBehavior: Clip.antiAlias,
         notchMargin: 6,
         child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: whiteColor,
-          elevation: 0,
-          selectedItemColor: blueColor,
-          unselectedItemColor: blackColor,
-          showSelectedLabels: true,
-          showUnselectedLabels: true,
-          selectedLabelStyle: blueTextStyle.copyWith(
-            fontSize: 10,
-            fontWeight: medium,
-          ),
-          unselectedLabelStyle: blackTextStyle.copyWith(
-            fontSize: 10,
-            fontWeight: medium,
-          ),
-          items: [
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic_overview.png',
-              color: blueColor,
-              width: 20,
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: whiteColor,
+            elevation: 0,
+            selectedItemColor: blueColor,
+            unselectedItemColor: blackColor,
+            showSelectedLabels: true,
+            showUnselectedLabels: true,
+            selectedLabelStyle: blueTextStyle.copyWith(
+              fontSize: 10,
+              fontWeight: medium,
             ),
-            label: "Overview",
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic_history.png',
-              width: 20,
+            unselectedLabelStyle: blackTextStyle.copyWith(
+              fontSize: 10,
+              fontWeight: medium,
             ),
-            label: "History",
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic_statistc.png',
-              width: 20,
-            ),
-            label: "Statistic",
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic_reward.png',
-              width: 20,
-            ),
-            label: "Reward",
-          ),
-        ]),
+            items: [
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/ic_overview.png',
+                  color: blueColor,
+                  width: 20,
+                ),
+                label: "Overview",
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/ic_history.png',
+                  width: 20,
+                ),
+                label: "History",
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/ic_statistc.png',
+                  width: 20,
+                ),
+                label: "Statistic",
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/ic_reward.png',
+                  width: 20,
+                ),
+                label: "Reward",
+              ),
+            ]),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () {},
         backgroundColor: purpleColor,
         shape: CircleBorder(),
         child: Image.asset(
           'assets/ic_plus_circle.png',
           width: 24,
-          ),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          BuildProfile(),
+        ],
+      ),
+    );
+  }
+
+  Widget BuildProfile() {
+    return Container(
+      margin: const EdgeInsets.only(top: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Howdy',
+                style: greyTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Text(
+                'shaynahan',
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: semibold,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: 60,
+            height: 60,
+            decoration: const BoxDecoration(
+              shape:  BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/img_profile.png',
+                  ), 
+                  )
+            ),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 16,
+                height: 16,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: whiteColor,
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.check_circle,
+                    color: greenColor,
+                    size: 14,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
