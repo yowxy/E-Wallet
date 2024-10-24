cf63f28e87cf911fd0b6a6c6117d9bb568396200
yowxy <iklilblender@gmail.com>
2024-10-25 01:05:03 +0700

    slicing pin page part 2

--- lib/ui/pages/pin_pages.dart
+++ lib/ui/pages/pin_pages.dart
@@ -1,6 +1,6 @@
 import "package:e_wallet/shared/theme.dart";
+import "package:e_wallet/ui/widgets/buttons.dart";
 import "package:flutter/material.dart";
-
 
 class PinPage extends StatelessWidget {
   const PinPage({super.key});
@@ -9,47 +9,146 @@ class PinPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: darkBackgroundColor,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Sha PIN',
-              style: whiteTextStyle.copyWith(
-                fontSize: 20,
-                fontWeight: semibold,
-              ),
-            ),
-            const SizedBox(
-              height: 72,
-            ),
-        SizedBox(
-          width: 200,
-          child: TextFormField(
-            obscureText: true,
-            obscuringCharacter: '*',
-            cursorColor: greyColor,
-            style: whiteTextStyle.copyWith(
-              fontSize: 36,
-              fontWeight: medium,
-              letterSpacing: 16,
-            ),
-            decoration: InputDecoration(
-              enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(
-                  color: greyColor,
-                ),
-              ),
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(
-                  color: greyColor, // Change this color if needed when the field is focused
-                ),
-              ),
-            ),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 58,
         ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Sha PIN',
+                style: whiteTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: semibold,
+                ),
+              ),
+              const SizedBox(
+                height: 72,
+              ),
+              SizedBox(
+                width: 200,
+                child: TextFormField(
+                  obscureText: true,
+                  obscuringCharacter: '*',
+                  cursorColor: greyColor,
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 36,
+                    fontWeight: medium,
+                    letterSpacing: 16,
+                  ),
+                  decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: greyColor,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color:
+                            greyColor, // Change this color if needed when the field is focused
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+        
+              const SizedBox(
+                height: 66,
+              ),
+        
+              Wrap(
+                spacing: 40,
+                runSpacing: 40,
+                children: [
 
-          ],
+                  CustomInputButton(
+                    title: '1',
+                    onTap: (){},
+                    ),
+
+
+                  CustomInputButton(
+                    title: '2',
+                    onTap: (){},
+                    ),
+
+
+                  CustomInputButton(
+                    title: '3',
+                    onTap: (){},
+                    ),
+
+
+                  CustomInputButton(
+                    title: '4',
+                    onTap: (){},
+                    ),
+
+
+                  CustomInputButton(
+                    title: '5',
+                    onTap: (){},
+                    ),
+
+
+                  CustomInputButton(
+                    title: '6',
+                    onTap: (){},
+                    ),
+
+
+                  CustomInputButton(
+                    title: '7',
+                    onTap: (){},
+                    ),
+
+
+                  CustomInputButton(
+                    title: '8',
+                    onTap: (){},
+                    ),
+
+
+                  CustomInputButton(
+                    title: '9',
+                    onTap: (){},
+                    ),
+
+                    const SizedBox(
+                      height: 60,
+                      width: 60,
+                    ),
+                    
+                    CustomInputButton(
+                      title: '0',
+                      onTap: (){},
+                      ),
+
+
+                  GestureDetector(
+                      onTap: (){},
+                      child: Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: numberBackgroundColor,
+                        ),
+                        child: Center(
+                          child: Icon(
+                            Icons.arrow_back,
+                            color: whiteColor,
+                          )
+                        ),
+                      ),
+                    )
+                ],
+              )
+        
+            ],
+          ),
         ),
       ),
     );
