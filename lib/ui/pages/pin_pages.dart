9e456b63f4b842310b3ce65c61e395f0908a5035
yowxy <iklilblender@gmail.com>
2024-10-25 01:47:22 +0700

    slicing pin page part 3

--- lib/ui/pages/pin_pages.dart
+++ lib/ui/pages/pin_pages.dart
@@ -2,8 +2,35 @@ import "package:e_wallet/shared/theme.dart";
 import "package:e_wallet/ui/widgets/buttons.dart";
 import "package:flutter/material.dart";
 
-class PinPage extends StatelessWidget {
+class PinPage extends StatefulWidget {
   const PinPage({super.key});
+
+  @override
+  State<PinPage> createState() => _PinPageState();
+}
+
+class _PinPageState extends State<PinPage> {
+
+
+  final TextEditingController pinController  = TextEditingController(text:  '');
+
+  addPin (String number){
+    if(pinController.text.length < 6){
+      setState(() {
+        pinController.text = pinController.text + number;
+      });
+    }
+    // print(pinController.text);
+  }
+
+  deletePin(){
+    if(pinController.text.isNotEmpty){
+      setState(() {
+        pinController.text = pinController.text.substring(0, pinController.text.length -1);
+      });
+    }
+    // print(pinController.text);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,8 +57,10 @@ class PinPage extends StatelessWidget {
               SizedBox(
                 width: 200,
                 child: TextFormField(
+                  controller: pinController,
                   obscureText: true,
                   obscuringCharacter: '*',
+                  enabled: false,
                   cursorColor: greyColor,
                   style: whiteTextStyle.copyWith(
                     fontSize: 36,
@@ -39,15 +68,9 @@ class PinPage extends StatelessWidget {
                     letterSpacing: 16,
                   ),
                   decoration: InputDecoration(
-                    enabledBorder: UnderlineInputBorder(
+                    disabledBorder: UnderlineInputBorder(
                       borderSide: BorderSide(
                         color: greyColor,
-                      ),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color:
-                            greyColor, // Change this color if needed when the field is focused
                       ),
                     ),
                   ),
@@ -65,55 +88,73 @@ class PinPage extends StatelessWidget {
 
                   CustomInputButton(
                     title: '1',
-                    onTap: (){},
+                    onTap: (){
+                      addPin('1');
+                    },
                     ),
 
 
                   CustomInputButton(
                     title: '2',
-                    onTap: (){},
+                    onTap: (){
+                      addPin('2');
+                    },
                     ),
 
 
                   CustomInputButton(
                     title: '3',
-                    onTap: (){},
+                    onTap: (){
+                      addPin('3');
+                    },
                     ),
 
 
                   CustomInputButton(
                     title: '4',
-                    onTap: (){},
+                    onTap: (){
+                      addPin('4');
+                    },
                     ),
 
 
                   CustomInputButton(
                     title: '5',
-                    onTap: (){},
+                    onTap: (){
+                      addPin('5');
+                    },
                     ),
 
 
                   CustomInputButton(
                     title: '6',
-                    onTap: (){},
+                    onTap: (){
+                      addPin('6');
+                    },
                     ),
 
 
                   CustomInputButton(
                     title: '7',
-                    onTap: (){},
+                    onTap: (){
+                      addPin('7');
+                    },
                     ),
 
 
                   CustomInputButton(
                     title: '8',
-                    onTap: (){},
+                    onTap: (){
+                      addPin('8');
+                    },
                     ),
 
 
                   CustomInputButton(
                     title: '9',
-                    onTap: (){},
+                    onTap: (){
+                      addPin('9');
+                    },
                     ),
 
                     const SizedBox(
@@ -123,12 +164,16 @@ class PinPage extends StatelessWidget {
                     
                     CustomInputButton(
                       title: '0',
-                      onTap: (){},
+                      onTap: (){
+                        addPin('0');
+                      },
                       ),
 
 
                   GestureDetector(
-                      onTap: (){},
+                      onTap: (){
+                        deletePin();
+                      },
                       child: Container(
                         width: 60,
                         height: 60,
